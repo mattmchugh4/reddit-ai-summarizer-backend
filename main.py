a1113e1e34a702d9fe3f160dff1a7510d66d6092
mattmchugh4 <114327452+mattmchugh4@users.noreply.github.com>
2023-04-06 16:57:33 -0500

    Working Http request, commented out socket code

--- main.py
+++ main.py
@@ -1,33 +1,62 @@
 from flask import Flask, request, jsonify
-from flask_socketio import SocketIO, emit
+# from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 
 from handle_query import start_query
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "*"}})
+# app.config['SECRET_KEY'] = '870c84b187e85d1350d36e0f5cfeffab26a9255e5bf5c3e3'
+# CORS(app, resources={r"/*": {"origins": "*"}})
+# socketio = SocketIO(app, cors_allowed_origins="*")
 
 
-@app.route('/api', methods=['POST'])
-def api():
-    # Get the request data
-    data = request.get_json()
+# Route for HTTP request
+@app.route("/http-call")
+def http_call():
+    # This function is executed when an HTTP GET request is made to the "/http-call" endpoint
+    # It returns a JSON object containing a single key-value pair, where the key is "data"
+    # and the value is a string.
+    data = {'data': 'my test'}
+    return jsonify(data)
 
-    # Do some processing with the data
-    response = process_request(data)
-
-    # Return the response
-    return response
+# Event listener for a client connecting to the server via Socket.IO
 
 
-def process_request(data):
-    # Process the request data here and return a response
-    response_data = "Your request was processed successfully!"
-    response = Response(response_data)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+# @socketio.on("connect")
+# def connected():
+#     # This function is executed when a new client connects to the server via Socket.IO
+#     # It prints the client's session ID and a message indicating that the client has connected.
+#     print(request.sid)
+#     print("client has connected")
+#     # Send a message to the client with their session ID
+#     emit("connect", {"data": f"id: {request.sid} is connected"})
+
+# # Event listener for receiving data from a client via Socket.IO
+
+
+# @socketio.on('data')
+# def handle_message(data):
+#     # This function is executed when the server receives a message from a client via Socket.IO
+#     # It prints the message received from the client and broadcasts the message to all connected clients,
+#     # along with the sender's session ID.
+#     print("data from the front end: ", str(data))
+#     emit("data", {'data': data, 'id': request.sid}, broadcast=True)
+
+# # Event listener for a client disconnecting from the server via Socket.IO
+
+
+# @socketio.on("disconnect")
+# def disconnected():
+#     # This function is executed when a client disconnects from the server via Socket.IO
+#     # It prints a message indicating that the user has disconnected and broadcasts a message
+#     # to all connected clients informing them of the disconnection.
+#     print("user disconnected")
+#     emit("disconnect", f"user {request.sid} disconnected", broadcast=True)
+
 
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(debug=True, port=5001)
+
+    # socketio.run(app, debug=True, port=5001)
