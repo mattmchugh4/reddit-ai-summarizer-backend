b5741cc1cd9dd9b9bcd2b9c04fa4d3c178081dd2
mattmchugh4 <114327452+mattmchugh4@users.noreply.github.com>
2023-04-12 12:30:00 -0500

    create websocket connection, change conda env

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-# from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 
 # Import the synchronous wrapper function
@@ -7,9 +7,9 @@ from handle_query import start_query
 
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = '870c84b187e85d1350d36e0f5cfeffab26a9255e5bf5c3e3'
-# CORS(app, resources={r"/*": {"origins": "*"}})
-# socketio = SocketIO(app, cors_allowed_origins="*")
+app.config['SECRET_KEY'] = 'secret!'
+CORS(app, resources={r"/*": {"origins": "*"}})
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 @app.errorhandler(ConnectionRefusedError)
@@ -30,40 +30,29 @@ def http_call():
         raise e
 
 
+@socketio.on("connect")
+def connected():
+    """event listener when client connects to the server"""
+    print(request.sid)
+    print("client has connected")
+    emit("connect", {"data": f"id: {request.sid} is connected"})
+
+
+@socketio.on('data')
+def handle_message(data):
+    """event listener when client types a message"""
+    print("data from the front end: ", str(data))
+    emit("data", {'data': data, 'id': request.sid}, broadcast=True)
+
+
+@socketio.on("disconnect")
+def disconnected():
+    """event listener when client disconnects to the server"""
+    print("user disconnected")
+    emit("disconnect", f"user {request.sid} disconnected", broadcast=True)
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    socketio.run(app, debug=True, port=5001)
 
-    # socketio.run(app, debug=True, port=5001)
-
-
-# @socketio.on("connect")
-# def connected():
-#     # This function is executed when a new client connects to the server via Socket.IO
-#     # It prints the client's session ID and a message indicating that the client has connected.
-#     print(request.sid)
-#     print("client has connected")
-#     # Send a message to the client with their session ID
-#     emit("connect", {"data": f"id: {request.sid} is connected"})
-
-# # Event listener for receiving data from a client via Socket.IO
-
-
-# @socketio.on('data')
-# def handle_message(data):
-#     # This function is executed when the server receives a message from a client via Socket.IO
-#     # It prints the message received from the client and broadcasts the message to all connected clients,
-#     # along with the sender's session ID.
-#     print("data from the front end: ", str(data))
-#     emit("data", {'data': data, 'id': request.sid}, broadcast=True)
-
-# # Event listener for a client disconnecting from the server via Socket.IO
-
-
-# @socketio.on("disconnect")
-# def disconnected():
-#     # This function is executed when a client disconnects from the server via Socket.IO
-#     # It prints a message indicating that the user has disconnected and broadcasts a message
-#     # to all connected clients informing them of the disconnection.
-#     print("user disconnected")
-#     emit("disconnect", f"user {request.sid} disconnected", broadcast=True)
+    # app.run(debug=True, port=5000)
